32f746682614957373d6eb455d22e085724a14e0
Kori <icedragon668@gmail.com>
2019-03-02 23:25:50 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -13,19 +13,33 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public")); //alter path as needed
 
-mongoose.connect('mongodb://localhost/kudos_db', {useNewUrlParser: true }) //what is this last bit?
-.then(()=>{
-    /* Reset and Seed DB
-    // mongoose.connection.db.dropDatabase();
+///
+let uri = 'mongodb://heroku_nhmm7mp8:39j5l2tco9e2gaao7jlcvd0426@ds211083.mlab.com:11083/heroku_nhmm7mp8/kudos_db';
 
-    // for (i=0;i<UserJSON.length;i++) {
-    //     User.create(UserJSON[i])
-    // };
+mongoose.connect(uri, {useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback() {
+
+/*local mongo
+//mongoose.connect('mongodb://localhost/kudos_db', {useNewUrlParser: true }) //what is this last bit?
+//.then(()=>{
+local mongo */
+
+    //* Reset and Seed DB
+    mongoose.connection.db.dropDatabase();
+
+    for (i=0;i<UserJSON.length;i++) {
+        User.create(UserJSON[i])
+    };
     
-    // for (i=0;i<KudosJSON.length;i++) {
-    //     Kudos.create(KudosJSON[i])
-    // };
-    */
+    for (i=0;i<KudosJSON.length;i++) {
+        Kudos.create(KudosJSON[i])
+    };
+    //*/
 }); 
 
 require('./routes/api-routes')(app);
@@ -34,26 +48,3 @@ require('./routes/api-routes')(app);
 app.listen(PORT, ()=>{
     console.log(`"Kudos" for finding ${PORT}`)
 });
-
-/*
-//this is a db.SQL server
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-const path = require('path');
-const db = require(path.join(__dirname, "./models"));
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(express.static("./public"));
-
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
-
-db.sequelize.sync({force:false}).then(function(){
-    app.listen(PORT, function(){
-        console.log(`Ears on ${PORT} good buddy`)
-    });
-});
-*/
